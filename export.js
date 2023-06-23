794b26bca3523dfc28603e56e756d606047fa8f0
DogDoge <132619375+DogDoge0-0@users.noreply.github.com>
2023-06-23 10:08:18 -0400

    Update export.js

--- export.js
+++ export.js
@@ -1,1 +1,12 @@
+// Initialization
+let CSV = document.querySelector('#exportCSV');
+let done = document.querySelector('#exportDone');
+let cancel = document.querySelector('#exportCancel');
+let modal2 = document.querySelector('#exportModal2');
+let exportRows = document.querySelectorAll('.tableRow');
 
+// Change the second modal
+$(modal2).modal({
+  backdrop: 'static'
+});
+$(modal2).draggable();
